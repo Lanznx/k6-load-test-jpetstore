127da4d3d90d61d4290706a91fac4509f10bc823
lanznx <109305092@g.nccu.edu.tw>
2023-05-07 22:39:37 +0800

    feat: insert prromql data to mysql

--- extract.py
+++ extract.py
@@ -1,37 +1,123 @@
 import re
-import csv
+import requests
+import json
+import mysql.connector
+import time
+import sys
 
-# 從文本中提取數據
-with open('./output/data.txt', 'r') as f:
-    data = f.read()
+args = sys.argv[1:]
+ENV_ID = args[0]
+ROUND = args[1]
+VU = args[2]
+TIME_RANGE = args[3]
+PATH_TO_TXT = args[4]
 
-results = re.findall(r'http_req_duration.+', data)
-match_success = re.findall(r"✓ (\d+)", data)
-match_fail = re.findall(r"✗ (\d+)", data) 
-# 將數據寫入 CSV 文件
-with open('./output/results.csv', 'w', newline='') as f:
-    writer = csv.writer(f, delimiter=',', lineterminator='')
-    checker = csv.writer(f)
-    writer.writerow(['error', 'avg', 'min', 'med', 'max', 'p(90)', 'p(95)\n'])
+mydb = mysql.connector.connect(
+  host="140.119.19.24",
+  user="root",
+  passwd="brandon",
+  database="mysql"
+)
 
-
-    rowCount = 0
-    checkCount = 0
-    for m in range(len(match_success)):
-        if(int(match_fail[m]) == 0):
-            writer.writerow([match_fail[m], ","])
-        else:
-            writer.writerow([str(int(match_fail[m]) / (int(match_success[m])+int(match_fail[m]) )), ","])
+mycursor = mydb.cursor()
 
 
-        for row in results:
-            if(rowCount < checkCount):
-                rowCount += 1
-                continue
-            values = re.findall(r'\d+\.\d+', row)
-            writer.writerow(values)
-            writer.writerow("\n")
+def main():
+    prometheus_url = 'http://jpetstore.cerana.tech:30000'
 
-            break
-        rowCount = 0
-        checkCount += 1
+    results, match_success, match_fail = clean_data(data)
+    error_rate = 100 * calculate_error_rate(match_success, match_fail)
+    response_time = get_response_time(results)
+
+    cpu_by_container_data, cpu_by_instance_data, memory_by_container_data, memory_by_instance_data = get_metrics(prometheus_url, TIME_RANGE)
+    metric = {
+        'env_id': ENV_ID,
+        'round': ROUND,
+        'VU': VU,
+        'error_rate': error_rate,
+        'response_time': response_time,
+        'cpu_container_jpetstore_backend': cpu_by_container_data[0]['value'][1],
+        'cpu_container_mysql': cpu_by_container_data[1]['value'][1],
+        'memory_container_jpetstore_backend': memory_by_container_data[0]['value'][1],
+        'memory_container_mysql': memory_by_container_data[1]['value'][1],
+        'cpu_instance_jpetstore_backend': cpu_by_instance_data[0]['value'][1],
+        'cpu_instance_mysql': cpu_by_instance_data[1]['value'][1],
+        'memory_instance_jpetstore_backend': memory_by_instance_data[0]['value'][1],
+        'memory_instance_mysql': memory_by_instance_data[1]['value'][1],
+        'timestamp': int(time.time())
+    }
+    insert_data_to_mysql(metric)
+
+
+
+with open(PATH_TO_TXT, 'r') as f:
+    data = f.readlines()[-2:]
+
+def clean_data(data):
+    data = ''.join(data)
+    results = re.findall(r'http_req_duration.+', data)[0]
+    match_success = re.findall(r"✓ (\d+)", data)[0]
+    match_fail = re.findall(r"✗ (\d+)", data)[0]
+    return results, match_success, match_fail
+
+def calculate_error_rate(match_success, match_fail):
+    if(int(match_fail) == 0):
+        error_rate = int(match_fail)
+    else:
+        error_rate = int(match_fail) / (int(match_success)+int(match_fail) )
+
+    if(error_rate > 1):
+        error_rate = 1
+    return error_rate
+
+def get_response_time(results):
+    values = re.findall(r'\d+\.\d+', results)
+    response_time = float(values[0])
+    if(response_time < 9):
+        response_time*=1000
+    return response_time
+
+def get_data_from_prometheus(promql, prometheus_url):
+    response = requests.get(
+        f'{prometheus_url}/api/v1/query',
+        params={
+            'query': promql
+        }
+    )
+    data = json.loads(response.text)
+    return data['data']['result']
+
+def get_metrics(prometheus_url, time_range):
+    cpu_by_container = f'sum(increase(container_memory_working_set_bytes{{container!="POD",container!="",namespace="jpetstore"}}[{time_range}m])) by (container)'
+    cpu_by_instance = f'sum(increase(container_cpu_usage_seconds_total{{container!="POD", container!="", namespace="jpetstore"}}[{time_range}m])) by (instance)'
+    memory_by_container = f'sum(increase(container_cpu_usage_seconds_total{{container!="POD", container!="", namespace="jpetstore"}}[{time_range}m])) by (container)'
+    memory_by_instance = f'sum(increase(container_memory_working_set_bytes{{container!="POD",container!="",namespace="jpetstore"}}[{time_range}m])) by (instance)'
+
+    cpu_by_container_data = get_data_from_prometheus(cpu_by_container, prometheus_url)
+    cpu_by_instance_data = get_data_from_prometheus(cpu_by_instance, prometheus_url)
+    memory_by_container_data = get_data_from_prometheus(memory_by_container, prometheus_url)
+    memory_by_instance_data = get_data_from_prometheus(memory_by_instance, prometheus_url)
+
+    return cpu_by_container_data, cpu_by_instance_data, memory_by_container_data, memory_by_instance_data
+
+
+
+def insert_data_to_mysql(data):
+    add_data = ("""
+    INSERT INTO k6.metric (
+        env_id, round, VU, error_rate, response_time,
+        `cpu_container_jpetstore-backend`, cpu_container_mysql, `memory_container_jpetstore-backend`,
+        memory_container_mysql, `cpu_instance_jpetstore-backend`, cpu_instance_mysql,
+        `memory_instance_jpetstore-backend`, memory_instance_mysql, timestamp
+    ) VALUES (
+        %(env_id)s, %(round)s, %(VU)s, %(error_rate)s, %(response_time)s,
+        %(cpu_container_jpetstore_backend)s, %(cpu_container_mysql)s, %(memory_container_jpetstore_backend)s,
+        %(memory_container_mysql)s, %(cpu_instance_jpetstore_backend)s, %(cpu_instance_mysql)s,
+        %(memory_instance_jpetstore_backend)s, %(memory_instance_mysql)s, %(timestamp)s
+    )
+    """)
+    mycursor.execute(add_data, data)
+    mydb.commit()
+
+if __name__ == '__main__':
+    main()
--- repeatInsert.sh
+++ repeatInsert.sh
@@ -1,8 +1,15 @@
+ENV_ID=3
 VU=100
+TIME_RANGE=2
+PATH_TO_TXT='./output/data_3.txt'
 
-for j in {1..6}
+IP=140.119.163.226
+
+sudo chmod 777 $PATH_TO_TXT
+
+for j in {1..50}
 do
-  echo "========== round $j VUs $VU ========== " >> ./output/data.txt
+  echo "========== round $j VUs $VU ========== " >> $PATH_TO_TXT
   for i in {1..10}
   do
     # cd /home/brandon/k6
@@ -11,17 +18,18 @@ do
     # sleep 10s
 
     cd /home/ansible/load-test-http/http-request/scripts
-    # sudo k6 run --vus $VU --duration 5m --out influxdb=http://localhost:8086/k6 ./register.js 
-    sudo k6 run --vus $VU --duration 5m ./register.js | grep -E "checks|http_req_duration" | tee -a ./output/data.txt
-    sleep 10s
+    sudo k6 run --vus $VU --duration 2m ./register.js | grep -E "checks|http_req_duration" | tee -a $PATH_TO_TXT
 
-    mysql -h 140.119.163.226 --port=30360 -u root -p'brandon' -e 'DELETE FROM jpetstore.ACCOUNT WHERE 1=1;'
+    mysql -h $IP --port=30360 -u root -p'brandon' -e 'DELETE FROM jpetstore.ACCOUNT WHERE 1=1;'
+    mysql -h $IP --port=30360 -u root -p'brandon' -e 'DELETE FROM jpetstore.PROFILE WHERE 1=1;'
+    mysql -h $IP --port=30360 -u root -p'brandon' -e 'DELETE FROM jpetstore.SIGNON WHERE 1=1;'
     
-    ssh cfliao@140.119.163.226 "k3s kubectl scale deployment jpetstore-backend-deployment --replicas=0 -n jpetstore"
-    ssh cfliao@140.119.163.226 "k3s kubectl scale deployment jpetstore-backend-deployment --replicas=1 -n jpetstore"
+    /home/brandon/miniconda3/bin/python ./extract.py $ENV_ID $i $VU $TIME_RANGE $PATH_TO_TXT
+
+    ssh cfliao@$IP "k3s kubectl scale deployment jpetstore-backend-deployment --replicas=0 -n jpetstore"
+    ssh cfliao@$IP "k3s kubectl scale deployment jpetstore-backend-deployment --replicas=3 -n jpetstore"
     sleep 150s
   done
-  python3 ./extract.py
   VU=$((VU+100))
 done
 
